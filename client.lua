26fe8b97c9e3056d4859b3dbfc46749802ea1bc7
Danboi <168639806+Danboi01@users.noreply.github.com>
2025-02-17 19:46:02 +1300

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,46 +1,44 @@
 local HUD_ELEMENTS = {
-    HUD = 0, 
-    HUD_WANTED_STARS = 1,
-    HUD_WEAPON_ICON = 2,
-    HUD_CASH = 3,
-    HUD_MP_CASH = 4,
-    HUD_MP_MESSAGE = 5,
-    HUD_VEHICLE_NAME = 6,
-    HUD_AREA_NAME = 7,
-    HUD_VEHICLE_CLASS = 8,
-    HUD_STREET_NAME = 9,
-    HUD_HELP_TEXT = 10,
-    HUD_FLOATING_HELP_TEXT_1 = 11,
-    HUD_FLOATING_HELP_TEXT_2 = 12,
-    HUD_CASH_CHANGE = 13,
-    HUD_RETICLE = 14,
-    HUD_SUBTITLE_TEXT = 15,
-    HUD_SAVING_GAME = 17,
-    HUD_GAME_STREAM = 18,
-    MAX_SCRIPTED_HUD_COMPONENTS = 141
+    0,  -- HUD
+    1,  -- HUD_WANTED_STARS
+    2,  -- HUD_WEAPON_ICON
+    3,  -- HUD_CASH
+    4,  -- HUD_MP_CASH
+    5,  -- HUD_MP_MESSAGE
+    6,  -- HUD_VEHICLE_NAME
+    7,  -- HUD_AREA_NAME
+    8,  -- HUD_VEHICLE_CLASS
+    9,  -- HUD_STREET_NAME
+    10, -- HUD_HELP_TEXT
+    11, -- HUD_FLOATING_HELP_TEXT_1
+    12, -- HUD_FLOATING_HELP_TEXT_2
+    13, -- HUD_CASH_CHANGE
+    14, -- HUD_RETICLE
+    15, -- HUD_SUBTITLE_TEXT
+    17, -- HUD_SAVING_GAME
+    18, -- HUD_GAME_STREAM
+    141 -- MAX_SCRIPTED_HUD_COMPONENTS
 }
 
-function SetData()
-    local name = GetPlayerName(PlayerId())
-    local id = GetPlayerServerId(PlayerId())
-    local textEntry = string.format('~b~[~w~Server #1~b~] ~w~| ~b~Player ID~w~: %s ~w~| ~b~Player Name~w~: %s', id, name)
-    Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), 'FE_THDR_GTAO', textEntry)
+local function UpdatePlayerData()
+    local playerName = GetPlayerName(PlayerId())
+    local playerId = GetPlayerServerId(PlayerId())
+    local textEntry = string.format(
+        "~b~[~w~Server #1~b~] ~w~| ~b~Player ID~w~: %d ~w~| ~b~Player Name~w~: %s",
+        playerId, playerName
+    )
+    AddTextEntry('FE_THDR_GTAO', textEntry)
 end
 
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(1000) 
-        SetData()
-    end
+CreateThread(function()
+    UpdatePlayerData()
 end)
 
-Citizen.CreateThread(function()
+CreateThread(function()
     while true do
-        Citizen.Wait(0)
-        for _, val in pairs(HUD_ELEMENTS) do 
-            if val ~= nil and type(val) == "number" then 
-                HideHudComponentThisFrame(val) 
-            end
+        Wait(0) 
+        for _, hud in ipairs(HUD_ELEMENTS) do
+            HideHudComponentThisFrame(hud)
         end
     end
 end)
